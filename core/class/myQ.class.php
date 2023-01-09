b17a025c5136c6c68804d87e1f2a5680f893a2c5
jeje-digifab <52994460+jeje-digifab@users.noreply.github.com>
2023-01-09 01:17:59 +0100

    Mise en place du plugin

--- core/class/myQ.class.php
+++ core/class/myQ.class.php
@@ -18,7 +18,7 @@
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
-class template extends eqLogic {
+class myQ extends eqLogic {
   /*     * *************************Attributs****************************** */
 
   /*
@@ -141,7 +141,7 @@ class template extends eqLogic {
 
 }
 
-class templateCmd extends cmd {
+class myQCmd extends cmd {
   /*     * *************************Attributs****************************** */
 
   /*
--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,13 +18,13 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 // Fonction exécutée automatiquement après l'installation du plugin
-function template_install() {
+function myQ_install() {
 }
 
 // Fonction exécutée automatiquement après la mise à jour du plugin
-function template_update() {
+function myQ_update() {
 }
 
 // Fonction exécutée automatiquement après la suppression du plugin
-function template_remove() {
+function myQ_remove() {
 }
